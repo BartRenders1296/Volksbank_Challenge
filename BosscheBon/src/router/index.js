3fe6f0c20159f30d24965fd2aaa7f5310124f875
Martijn van de Wetering <magweter@avans.nl>
2018-03-07 19:38:55 +0100

    - Added google map

--- BosscheBon/src/router/index.js
+++ BosscheBon/src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
+import Map from '@/components/Map'
+import Maps from '@/components/Maps'
 
 Vue.use(Router)
 
@@ -10,6 +12,16 @@ export default new Router({
       path: '/',
       name: 'Hello',
       component: Hello
+    },
+    {
+      path: '/map',
+      name: 'Map',
+      component: Map
+    },
+    {
+      path: '/maps',
+      name: 'Maps',
+      component: Maps
     }
   ]
 })
